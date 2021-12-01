7d2a39cbe3978e04d4df03d73ee780cf11416a22
Team-4585 <huskyrobotics1@gmail.com>
2021-12-01 15:45:59 -0800

    Created abstract class for Wall, created specific AmesWall class

---
+++ AmesOffenseRobot/src/main/java/frc/robot/subsystem/AmesWall.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystem;
+
+public class AmesWall extends Wall {
+    
+    public AmesWall (double motorSpeed, MotorController motorController) {
+        super(motorSpeed, motorController);
+    }
+
+    public void moveUp() {
+
+    }
+
+    public void moveDown() {
+
+    }
+
+    public void moveLeft() {
+
+    }
+
+    public void moveRight() {
+
+    }
+
+    public void getPosition() {
+        
+    }
+
+
+}
